e7cffa98d94af00d55c1ffb79bf156cf8ae963f5
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-09-29 22:16:00 +0200

    badagoAktorea metodoa gehitu

--- src/filmdb/ListaAktoreak.java
+++ src/filmdb/ListaAktoreak.java
@@ -12,7 +12,13 @@ public class ListaAktoreak {
 	}
 
 	public void gehituAktorea(Aktorea p) {
-		this.aktoreak.add(p);
+		if(not(badagoAktorea(p))) this.aktoreak.add(p);
+		//TODO errore mezua gehitu
+	}
+
+	public boolean badagoAktorea(Aktorea p) {
+		if(this.aktoreak.contains(p)) return true;
+		else false;
 	}
 
 }
