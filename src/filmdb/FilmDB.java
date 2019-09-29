5e9ea9dccebc2c780004d59c9249718337b16090
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-09-29 14:55:46 +0200

    aldaketa txikiak

---
+++ src/filmdb/FilmDB.java
@@ -0,0 +1,19 @@
+package filmdb;
+
+public class FilmDB {
+	private ListaFilmak filmak;
+	private static filmDB gureDB;
+
+	private FilmDB sortufilmDB() {
+		this.filmak = new ListaFilmak();
+	}
+
+	public static Filmdb getFilmDB() {
+		}
+		return gureDB;
+	}
+
+	public String datuakKargatu() {
+		// String a = linea.split("\\s+-->");
+	}
+}
