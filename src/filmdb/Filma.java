6832d97861064bb2e0f45811e1686d89e506564e
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-09-29 14:52:30 +0200

    metodo berriak sortu

---
+++ src/filmdb/Filma.java
@@ -0,0 +1,36 @@
+package filmdb;
+
+public class Filma {
+	private String izena;
+	private ListaAktoreak aktoreak;
+	private int dirua;
+	
+	public Filma() {
+		this.aktoreak=new ListaAktoreak();
+	}
+
+	public ListaAktoreak getAktoreak() {
+		return this.aktoreak;
+	}
+	
+	public String getIzena() {
+		return this.izena;
+	}
+
+	public void gehituAktorea(Aktorea p) {
+		this.aktoreak.add(p);
+	}
+
+	public void kenduAktorea(Aktorea p) {
+		this.aktoreak.remove(p);
+	}
+
+	public void gehituDirua(int d) {
+		this.dirua = this.dirua + d;
+	}
+
+	public void ordenatuAktoreak() {
+		//TODO
+	}
+
+}
