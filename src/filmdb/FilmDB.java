6530a8f0c130fd2f1a5fb6f12225fcd19f15535e
jrenteria003 <jrenteria003@ikasle.ehu.eus>
2019-09-29 22:57:15 +0200

    main() gehitu eta datuakKargatu() garatu

--- src/filmdb/FilmDB.java
+++ src/filmdb/FilmDB.java
@@ -8,12 +8,28 @@ public class FilmDB {
 		this.filmak = new ListaFilmak();
 	}
 
+	public int main() {
+		datuakKargatu();
+		return 0;
+
 	public static Filmdb getFilmDB() {
-		}
 		return gureDB;
 	}
 
-	public String datuakKargatu() {
+	public void datuakKargatu(String fitxategia) {
 		// String a = linea.split("\\s+-->");
+		try{
+			Scanner entrada = new Scanner(new FileReader(nomF));
+			String linea;
+			Filma f = null;
+			while (entrada.hasNext()) {
+				linea = entrada.nextLine();
+				String iz = linea.split("\\s+--->");
+				f = new Filma(iz, di);
+				//f.gehituAktorea(p);
+			}
+			entrada.close();
+		}
+		catch(IOException e) {e.printStackTrace();}
 	}
 }
