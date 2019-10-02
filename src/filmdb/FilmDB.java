4f9872ac1ea463fc7ec26e25bb9c8b06c80720e8
jonr <jrenteria003@ikasle.ehu.eus>
2019-10-02 19:23:15 +0200

    kodea garbitu

--- src/filmdb/FilmDB.java
+++ src/filmdb/FilmDB.java
@@ -13,6 +13,7 @@ public class FilmDB {
 	private ListaFilmak filmak;
 	private static FilmDB gureDB;
 	private HashMap<String,Aktorea> mapa;
+	private HashMap<String, Filma> mapaFilmak;
 
 	private FilmDB() {
 		this.filmak = new ListaFilmak();
@@ -28,8 +29,6 @@ public class FilmDB {
 	}
 
 	public static void datuakKargatu(String nomF) {
-		// String a = linea.split("\\s+-->");
-
 		try{
 			Scanner entrada = new Scanner(new FileReader(nomF));
 			String linea;
@@ -40,8 +39,6 @@ public class FilmDB {
 				linea = entrada.nextLine();
 				String[] iz = linea.split("\\s+--->\\s+");
 				f = new Filma(iz[0]);
-				//aktors = new ListaAktoreak();
-				//aktors.gehitu(p);
 				String[] akt = iz[1].split("\\s+&&&\\s+");
 				System.out.println(iz[0]);
 				for(int i = 0; i < akt.length; i++) {
@@ -55,10 +52,5 @@ public class FilmDB {
 			entrada.close();
 		}
 		catch(IOException e) {e.printStackTrace();}
-
-
-		//InputStream fitx = (InputStream) this.getClass().getClassLoader().getResourceAsStream(fitxategia);
-
-		//InputStreamReader in = new InputStreamReader(fitx);
 	}
 }
