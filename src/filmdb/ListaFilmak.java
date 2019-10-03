19ff29d8a0c1eabf194e4723045e1797d8dbca42
jonr <jrenteria003@ikasle.ehu.eus>
2019-10-04 00:24:31 +0200

    ArrayListak erabili berriro eta EMA-tik DMA-ra pasatu

--- src/filmdb/ListaFilmak.java
+++ src/filmdb/ListaFilmak.java
@@ -1,40 +1,27 @@
 package filmdb;
 
-//import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.ArrayList;
+//import java.util.HashMap;
 
 public class ListaFilmak {
-	private  HashMap<String,Filma> mapaFilmak;
-	
+	//private HashMap<String,Filma> mapaFilmak;
+	private ArrayList<Filma> filmak;
+
 	public ListaFilmak() {
-		this.mapaFilmak=new HashMap<String,Filma>();
+		this.filmak = new ArrayList<Filma>();
+		//this.mapaFilmak=new HashMap<String,Filma>();
 	}
 
-	public void sortuListaFilmak() { //no se que es esto
-		this.mapaFilmak = null;
-	}
-
-	public HashMap<String,Filma> getFilmak() {
-		return this.mapaFilmak;
-	}
+	//public HashMap<String,Filma> getFilmak() {
+	//	return this.mapaFilmak;
+	//}
 
 	public void gehituFilma(Filma f) {
-		if (!(this.mapaFilmak.containsKey(f.getIzena()))){
-			this.mapaFilmak.put(f.getIzena(),f);
-		}
-		//else {
-		//	throw new Exception("badago aktorea jadanik"); //ez dakit hau ondo dagoen
-		//}
-		//this.filmak.add(f);
+		if(!this.filmak.contains(f)) this.filmak.add(f);
 	}
 
 	public void kenduFilma(Filma f) {
-		if (this.mapaFilmak.containsKey(f.getIzena())){
-			this.mapaFilmak.remove(f.getIzena());
-		}
-		//else {
-		//	throw new Exception("ez dago aktore hau"); //ez dakit hau ondo dagoen
-		//}
+		if(!this.filmak.contains(f)) this.filmak.remove(f);
 	}
 
 }
