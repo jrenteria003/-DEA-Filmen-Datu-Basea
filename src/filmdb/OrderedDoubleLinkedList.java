2a9c37fd2fbcc1de36f79a933278ed453f0da25f
jonr <jrenteria003@ikasle.ehu.eus>
2019-10-21 13:27:20 +0200

    metodoak inplementatu

--- src/filmdb/OrderedDoubleLinkedList.java
+++ src/filmdb/OrderedDoubleLinkedList.java
@@ -3,10 +3,20 @@ package filmdb;
 public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements OrderedListADT<T> {
 	
 	public void add(T elem){
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
-
-
+		// KOSTUA = O(n)
+		Node<T> unekoa = this.first;
+		boolean sartuDa = false;
+		while(unekoa != this.first && !sartuDa) {
+			if(!sartuDa) sartuDa = true;
+			if(unekoa.data.toString().compareTo(elem.toString()) >= 0) {
+				sartuDa = false;
+				Node<T> berria = new Node<T>(elem);
+				berria.next = unekoa.next;
+				berria.prev = unekoa;
+				break;
+			}
+			unekoa = unekoa.next;
+		}
 	}
 
 	public void merge(DoubleLinkedList<T> zerrenda){
--- src/filmdb/UnorderedDoubleLinkedList.java
+++ src/filmdb/UnorderedDoubleLinkedList.java
@@ -1,21 +1,45 @@
 package filmdb;
 
 public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
-	
-	public void addToFront(T elem) {
-	// hasieran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
 
+	public void addToFront(T elem) {
+		// hasieran gehitu
+		// KOSTUA = O(1)
+		Node<T> berria = new Node<T>(elem);
+		Node<T> lehen = this.first;
+		this.first = berria;
+		berria.next = lehen;
+		berria.prev = lehen.prev;
+		lehen.prev = berria;
 	}
 
 	public void addToRear(T elem) {
-	// bukaeran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
+		// bukaeran gehitu
+		// KOSTUA = O(1)
+		Node<T> berria = new Node<T>(elem);
+		Node<T> azkena = this.first.prev;
+		azkena.next = berria;
+		this.first.prev = berria;
+		berria.next = this.first;
+		berria.prev = azkena;
 	}
-	
+
 	public void addAfter(T elem, T target) {
 		// KODEA OSATU ETA KOSTUA KALKULATU (AUKERAZKOA)
+		// KOSTUA = O(n)
+		Node<T> unekoa = this.first;
+		boolean sartuDa = false;
+		while(unekoa != this.first && !sartuDa) {
+			if(!sartuDa) sartuDa = true;
+			if(unekoa.data.equals(target)) {
+				Node<T> berria = new Node<T>(elem);
+				berria.next = unekoa.next.next;
+				unekoa.next = berria;
+				berria.prev = unekoa;
+				sartuDa = false;
+				break;
+			}
+			unekoa = unekoa.next;
+		}
 	}
-
 }
